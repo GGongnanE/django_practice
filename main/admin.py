1844e4e04dba2b688f71159cf7bf6fc7d8581164
GGNE <zzlzlzl007@gmail.com>
2022-05-25 14:47:00 +0900

    model, admin 관련 코드 추가

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+# admin.site.register(Board)
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'like_count']
+    list_display_links = ['title','like_count']
--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=100)
+    like_count = models.IntegerField()
+    view_count = models.IntegerField()
+    contents = models.TextField()
+
+    def __str__(self):
+        return self.title
